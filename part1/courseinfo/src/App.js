6c19dc1db3bffca40b4726089d217222accf3472
Xinshui Komorebi <33796087+Xinshui123@users.noreply.github.com>
2023-05-28 13:07:36 +0000

    add part1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,75 @@
+const Header = (props) => {
+  const course = props.course
+  return (
+    <div>
+      <p>
+        {course}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  const name1 = props.name1
+  const name2 = props.name2
+  const name3 = props.name3
+  const num1 = props.num1
+  const num2 = props.num2
+  const num3 = props.num3
+
+  const Part = (props) => {
+    const names = names
+    const nums = nums
+    <div>
+          <p>
+      {names},{nums}
+    </p>
+    </div>
+
+  }
+  return (
+    <div>
+      <Part name = {name1} num = {num1} />
+      <Part name = {name2} num = {num2} />
+      <Part name = {name3} num = {num3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.total
+  return (
+    <div>
+      <p>
+        Total is {total}
+      </p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        name1={part1}
+        name2={part2}
+        name3={part3}
+        num1={exercises1}
+        num2={exercises2}
+        num3={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+export default App

889daafeede2f987e096a04631135e318cc55d7e
Xinshi123 <a13418404439@gmail.com>
2023-08-28 21:25:02 +0800

    add axios and json server

--- part2/ThePhonebook/src/App.js
+++ part2/ThePhonebook/src/App.js
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import PersonShow from './components/PersonShow'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1, important: true },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2, important: true },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3, important: true },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4, important: true }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleChange = (event) => {
     setSearch(event.target.value)

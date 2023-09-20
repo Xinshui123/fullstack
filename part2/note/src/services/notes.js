7a86a8566ac842724d9374ceac57a75fb6b5d1b7
Xinshi123 <a13418404439@gmail.com>
2023-09-20 19:29:39 +0800

    go to cloud

--- part2/note/src/services/notes.js
+++ part2/note/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = async () => {
     const request = axios.get(baseUrl)
